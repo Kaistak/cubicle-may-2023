197f21a63098155e8b9ca66e6e4fe919e86ab52c
Kaistak <134406098+Kaistak@users.noreply.github.com>
2023-06-02 20:23:55 +0300

    Show attached accessories

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -19,7 +19,8 @@ exports.getAll = async (search, from, to) => {
 };
 
 
-exports.getOne = (cubeId) => Cube.findById(cubeId)
+exports.getOne = (cubeId) => Cube.findById(cubeId);
+exports.getOneWithAccessories = (cubeId) => this.getOne(cubeId).populate('accessories');
 
 exports.create =  (cubeData) => {
     const cube = new Cube(cubeData);
