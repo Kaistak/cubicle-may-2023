e8d811b9e805353a959ba85bf781abad89392c75
Kaistak <134406098+Kaistak@users.noreply.github.com>
2023-06-02 19:13:17 +0300

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const expresssConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-const routes = require('./routes')
+const dbConnect= require('./config/dbConfig');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
 
 expresssConfig(app);
 handlebarsConfig(app);
-app.use(routes);
+dbConnect()
+    .then(() => console.log('Db Connected sucssesfully'))
+    .catch((err) => console.log('Db err', err))
 
+app.use(routes);
 
 app.listen(PORT, () => console.log('Server is runnig on port 5000...'));
